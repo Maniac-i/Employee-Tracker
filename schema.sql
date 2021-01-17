aad9260c74ee0174bfb2668312f3809a6988f570
Joe Maniaci <joe.maniaci1@gmail.com>
2021-01-17 12:44:40 -0500

    created schema for cms_db

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS cms_db;
+CREATE DATABASE cms_db;
+
+USE cms_db;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department (id) 
+);
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role (id)
+);
